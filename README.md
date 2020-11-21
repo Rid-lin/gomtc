1d50601904e595a44e5c139fa7db316febf76ec5
Rid-lin <vlad@vegner.org>
2020-11-21 12:29:47 +0500

    Satisfied go-lint. Added graceful shutdown.

---
+++ README.md
@@ -0,0 +1,89 @@
+# Minimalist Netflow v5 to influxdb UDP collector written in Go 
+
+Forcked by https://github.com/strzinek/gonflux
+Broker listens on specified UDP port (2055 by default), accepting Netflow traffic, and collecting records with selected metadata formatted in line protocol to UDP listener of [influxdb](https://github.com/influxdata/influxdb).
+
+Project includes dockerfile for building runtime application as [docker](https://www.docker.com) container and also [Gitlab CI](https://about.gitlab.com/product/continuous-integration) definition file both for pushing build to docker registry and also for deploying to production docker server.
+
+To enable UDP listener for influxdb, either modify `influxd.conf` file and add:
+
+```
+[[udp]]
+enabled = true
+bind-address = ":8089"
+database = "db_for_netflow"
+```
+
+or set following environment variables:
+
+```
+INFLUXDB_UDP_ENABLED
+INFLUXDB_UDP_BIND_ADDRESS
+INFLUXDB_UDP_DATABASE
+```
+
+Netflow data in influxdb can then be analyzed for example with help of [Grafana](https://grafana.com).
+
+**BEWARE!!!**
+
+The cardinality of general Netflow data is quite high, so to use influxdb effectively for it, I suggest using several retention policies and downsampling the measurements.
+
+PLAN FIRST! ;-)
+
+## Usage
+
+```
+gonflux -method udp -out influxdb.local:8089
+```
+
+Supported command line parameters:
+```
+  -buffer int
+        Size of RxQueue, i.e. value for SO_RCVBUF in bytes (default 212992)
+  -in string
+        Address and port to listen for NetFlow packets (default "0.0.0.0:2055")
+  -method string
+        Output method: stdout, udp (default "stdout")
+  -out string
+        Address and port of influxdb to send decoded data
+```
+
+Or with docker container: 
+
+```
+docker run --name=gonflux -t --restart=always -p 2055:2055/udp -e METHOD='udp' -e OUT='influxdb.local:8089' -d strzinek/gonflux
+```
+
+## Credits
+
+This project was created with help of:
+
+*  https://github.com/chemidy/smallest-secured-golang-docker-image
+*  https://github.com/yunazuno/nfcollect
+
+## Licence
+**MIT**
+
+Copyright (c) 2019 Pavel Strzinek
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
---
+++ main.go
@@ -0,0 +1,291 @@
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+)
+
+// NetFlow v5 implementation
+
+type header struct {
+	Version          uint16
+	FlowRecords      uint16
+	Uptime           uint32
+	UnixSec          uint32
+	UnixNsec         uint32
+	FlowSeqNum       uint32
+	EngineType       uint8
+	EngineID         uint8
+	SamplingInterval uint16
+}
+
+type binaryRecord struct {
+	Ipv4SrcAddrInt uint32
+	Ipv4DstAddrInt uint32
+	Ipv4NextHopInt uint32
+	InputSnmp      uint16
+	OutputSnmp     uint16
+	InPkts         uint32
+	InBytes        uint32
+	FirstInt       uint32
+	LastInt        uint32
+	L4SrcPort      uint16
+	L4DstPort      uint16
+	_              uint8
+	TCPFlags       uint8
+	Protocol       uint8
+	SrcTos         uint8
+	SrcAs          uint16
+	DstAs          uint16
+	SrcMask        uint8
+	DstMask        uint8
+	_              uint16
+}
+
+type decodedRecord struct {
+	header
+	binaryRecord
+
+	Host              string
+	SamplingAlgorithm uint8
+	Ipv4SrcAddr       string
+	Ipv4DstAddr       string
+	Ipv4NextHop       string
+	SrcHostName       string
+	DstHostName       string
+	Duration          uint16
+}
+
+func intToIPv4Addr(intAddr uint32) net.IP {
+	return net.IPv4(
+		byte(intAddr>>24),
+		byte(intAddr>>16),
+		byte(intAddr>>8),
+		byte(intAddr))
+}
+
+func decodeRecord(header *header, binRecord *binaryRecord, remoteAddr *net.UDPAddr) decodedRecord {
+
+	decodedRecord := decodedRecord{
+
+		Host: remoteAddr.IP.String(),
+
+		header: *header,
+
+		binaryRecord: *binRecord,
+
+		Ipv4SrcAddr: intToIPv4Addr(binRecord.Ipv4SrcAddrInt).String(),
+		Ipv4DstAddr: intToIPv4Addr(binRecord.Ipv4DstAddrInt).String(),
+		Ipv4NextHop: intToIPv4Addr(binRecord.Ipv4NextHopInt).String(),
+		Duration:    uint16((binRecord.LastInt - binRecord.FirstInt) / 1000),
+	}
+
+	// LookupAddr
+	decodedRecord.SrcHostName = lookUpWithCache(decodedRecord.Ipv4SrcAddr)
+	decodedRecord.DstHostName = lookUpWithCache(decodedRecord.Ipv4DstAddr)
+
+	// decode sampling info
+	decodedRecord.SamplingAlgorithm = uint8(0x3 & (decodedRecord.SamplingInterval >> 14))
+	decodedRecord.SamplingInterval = 0x3fff & decodedRecord.SamplingInterval
+
+	return decodedRecord
+}
+
+func pipeOutputToStdout(outputChannel chan decodedRecord) {
+	var record decodedRecord
+	for {
+		record = <-outputChannel
+		out, _ := json.Marshal(record)
+		fmt.Println(string(out))
+	}
+}
+
+type cacheRecord struct {
+	Hostname string
+	timeout  time.Time
+}
+
+var (
+	cache      = map[string]cacheRecord{}
+	cacheMutex = sync.RWMutex{}
+)
+
+func lookUpWithCache(ipAddr string) string {
+	hostname := ipAddr
+	cacheMutex.Lock()
+	hostnameFromCache := cache[ipAddr]
+	cacheMutex.Unlock()
+	if (hostnameFromCache == cacheRecord{} || time.Now().After(hostnameFromCache.timeout)) {
+		hostTemp, err := net.LookupAddr(ipAddr)
+		if err == nil {
+			hostname = hostTemp[0]
+		}
+		cacheMutex.Lock()
+		cache[ipAddr] = cacheRecord{hostname, time.Now().AddDate(0, 0, 1)}
+		cacheMutex.Unlock()
+	} else {
+		hostname = hostnameFromCache.Hostname
+	}
+	return hostname
+}
+
+func formatLineProtocol(record decodedRecord) []byte {
+	return []byte(fmt.Sprintf("netflow,host=%s,srcAddr=%s,dstAddr=%s,srcHostName=%s,dstHostName=%s,protocol=%d,srcPort=%d,dstPort=%d,input=%d,output=%d inBytes=%d,inPackets=%d,duration=%d %d",
+		record.Host, record.Ipv4SrcAddr, record.Ipv4DstAddr, record.SrcHostName, record.DstHostName, record.Protocol, record.L4SrcPort, record.L4DstPort, record.InputSnmp, record.OutputSnmp,
+		record.InBytes, record.InPkts, record.Duration,
+		uint64((uint64(record.UnixSec)*uint64(1000000000))+uint64(record.UnixNsec))))
+}
+
+func pipeOutputToUDPSocket(outputChannel chan decodedRecord, targetAddr string) {
+	/* Setting-up the socket to send data */
+
+	remote, err := net.ResolveUDPAddr("udp", targetAddr)
+	if err != nil {
+		log.Printf("Name resolution failed: %v\n", err)
+	} else {
+
+		for {
+			connection, err := net.DialUDP("udp", nil, remote)
+			if err != nil {
+				log.Printf("Connection failed: %v\n", err)
+			} else {
+				defer connection.Close()
+				var record decodedRecord
+				for {
+					record = <-outputChannel
+					var buf = formatLineProtocol(record)
+					conn := connection
+					conn.SetDeadline(time.Now().Add(3 * time.Second))
+					_, err := conn.Write(buf)
+					if err != nil {
+						log.Printf("Send Error: %v\n", err)
+						break
+					}
+				}
+			}
+		}
+	}
+}
+
+func handlePacket(buf *bytes.Buffer, remoteAddr *net.UDPAddr, outputChannel chan decodedRecord) {
+	header := header{}
+	err := binary.Read(buf, binary.BigEndian, &header)
+	if err != nil {
+		log.Printf("Error: %v\n", err)
+	} else {
+
+		for i := 0; i < int(header.FlowRecords); i++ {
+			record := binaryRecord{}
+			err := binary.Read(buf, binary.BigEndian, &record)
+			if err != nil {
+				log.Printf("binary.Read failed: %v\n", err)
+				break
+			}
+
+			decodedRecord := decodeRecord(&header, &record, remoteAddr)
+			outputChannel <- decodedRecord
+		}
+	}
+}
+
+func getExitSignalsChannel() chan os.Signal {
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c,
+		// https://www.gnu.org/software/libc/manual/html_node/Termination-Signals.html
+		syscall.SIGTERM, // "the normal way to politely ask a program to terminate"
+		syscall.SIGINT,  // Ctrl+C
+		syscall.SIGQUIT, // Ctrl-\
+		// syscall.SIGKILL, // "always fatal", "SIGKILL and SIGSTOP may not be caught by a program"
+		syscall.SIGHUP, // "terminal is disconnected"
+	)
+	return c
+
+}
+
+func main() {
+	/* Parse command-line arguments */
+	var (
+		inSource               string
+		outMethod              string
+		outDestination         string
+		receiveBufferSizeBytes int
+		conn                   *net.UDPConn
+		err                    error
+	)
+	flag.StringVar(&inSource, "in", "0.0.0.0:2055", "Address and port to listen NetFlow packets")
+	flag.StringVar(&outMethod, "method", "stdout", "Output method: stdout, udp")
+	flag.StringVar(&outDestination, "out", "", "Address and port of influxdb to send decoded data")
+	flag.IntVar(&receiveBufferSizeBytes, "buffer", 212992, "Size of RxQueue, i.e. value for SO_RCVBUF in bytes")
+	flag.Parse()
+
+	/*Creating a channel to intercept the program end signal*/
+	exitChan := getExitSignalsChannel()
+
+	go func() {
+		<-exitChan
+		conn.Close()
+		log.Println("Shutting down")
+		os.Exit(0)
+
+	}()
+
+	/* Create output pipe */
+	outputChannel := make(chan decodedRecord, 100)
+	switch outMethod {
+	case "stdout":
+		go pipeOutputToStdout(outputChannel)
+	case "udp":
+		go pipeOutputToUDPSocket(outputChannel, outDestination)
+	default:
+		log.Fatalf("Unknown schema: %v\n", outMethod)
+
+	}
+
+	/* Start listening on the specified port */
+	log.Printf("Start listening on %v and sending to %v %v\n", inSource, outMethod, outDestination)
+	addr, err := net.ResolveUDPAddr("udp", inSource)
+	if err != nil {
+		log.Fatalf("Error: %v\n", err)
+	}
+
+	for {
+		conn, err = net.ListenUDP("udp", addr)
+		if err != nil {
+			log.Errorln(err)
+		} else {
+			err = conn.SetReadBuffer(receiveBufferSizeBytes)
+			if err != nil {
+				log.Errorln(err)
+			} else {
+				defer conn.Close()
+				/* Infinite-loop for reading packets */
+				for {
+					buf := make([]byte, 4096)
+					rlen, remote, err := conn.ReadFromUDP(buf)
+
+					if err != nil {
+						log.Errorf("Error: %v\n", err)
+					} else {
+
+						stream := bytes.NewBuffer(buf[:rlen])
+
+						go handlePacket(stream, remote, outputChannel)
+					}
+				}
+			}
+		}
+
+	}
+}
