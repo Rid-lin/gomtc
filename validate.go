ab83cf27b68d3f7e40a63b15ad6d8601d443ed3c
Rid-lin <vlad@vegner.org>
2021-05-25 20:18:51 +0500

    Added the function of checking the presence of a group (address-list) in the list of groups (address-lists)

--- validate.go
+++ validate.go
@@ -112,3 +112,13 @@ func isComment(inputStr, parametr string) bool {
 	}
 	return true
 }
+
+func isBlocked(addressLists, blockGroup string) bool {
+	arr := strings.Split(addressLists, ",")
+	for _, item := range arr {
+		if item == blockGroup {
+			return true
+		}
+	}
+	return false
+}
