1914cc05381a98c88a47a4a88c2bcc26cee87042
Rid-lin <vlad@vegner.org>
2021-05-26 17:19:43 +0500

    Fixed: too many unlocks. Added: Formating ouput blocking/unblocking

--- quota.go
+++ quota.go
@@ -77,18 +77,19 @@ func (t *Transport) checkQuotas() {
 			// alias.TimeoutBlock = setDailyTimeout()
 			alias.addBlockGroup(p.BlockAddressList)
 			t.change = append(t.change, alias)
-			log.Debugf("Login (%v) was disabled due to exceeding the daily quota(%v)", alias.Alias, alias.DailyQuota)
+			log.Debugf("Login (%18s) was disabled due to exceeding the daily quota(%v)", alias.Alias, alias.DailyQuota)
 		case alias.SizeOfHour[hour] >= alias.HourlyQuota && alias.DateStr == tn && !alias.Blocked:
 			alias.ShouldBeBlocked = true
 			// alias.TimeoutBlock = setHourlyTimeout()
 			alias.addBlockGroup(p.BlockAddressList)
 			t.change = append(t.change, alias)
-			log.Debugf("Login (%v) was disabled due to exceeding the hourly quota(%v)", alias.Alias, alias.DailyQuota)
-		case alias.Blocked:
+			log.Debugf("Login (%18s) was disabled due to exceeding the hourly quota(%v)", alias.Alias, alias.DailyQuota)
+		case alias.Blocked && alias.DateStr == tn:
 			alias.ShouldBeBlocked = false
 			alias.delBlockGroup(p.BlockAddressList)
 			t.change = append(t.change, alias)
-			log.Debugf("Login (%v)has been enabled, the quota(%v) has not been exceeded", alias.Alias, alias.HourlyQuota)
+			log.Debugf("Login (%18s) has been enabled, the quota(%v) has not been exceeded(Blocked:%v)",
+				alias.Alias, alias.HourlyQuota, alias.Blocked)
 		}
 		t.dataOld[key] = alias
 
